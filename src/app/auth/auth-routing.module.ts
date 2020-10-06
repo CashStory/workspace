bea992e018ef990b1ffc97f775ea7646b108d0b3
riderx <martindonadieu@gmail.com>
2020-10-06 11:02:47 +0200

    feat: :sparkles: add register form

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
 import { BaseComponent } from './base/base';
 
@@ -13,6 +14,10 @@ const routes: Routes = [{
     {
       path: 'login',
       component: LoginComponent,
+    },
+    {
+      path: 'register',
+      component: RegisterComponent,
     },
     {
       path: 'logout',
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,14 +8,13 @@ import { environment } from '../../../environments/environment';
 import platformDetect from 'platform-detect';
 
 @Component({
-  selector: 'ngx-app-login',
+  selector: 'ngx-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   user: ILogin = { username: null, password: null };
   sub: any;
-  landingUrl = environment.landingUrl;
   passwordType = 'password';
   web = platformDetect.web;
   activeSamlAuth: [] = [];
